b9ed845d871efdfacc05a5688ef82cf74a07fee4
astromai <ubilla.tomas012@gmail.com>
2025-05-26 22:38:12 -0400

    feat: add 'clean_board' and 'create_board' functions

--- tarea1/board.sh
+++ tarea1/board.sh
@@ -1,21 +1,43 @@
 random_text() {
     local length=$1
-    cat /dev/urandom | tr -dc [a-zA-Z0-9]
+    cat /dev/urandom | tr -dc [a-zA-Z0-9] | head -c "$length"
+}
+
+clean_board() {
+    rm -rf "$dir"
 }
 
 create_board() {
-    local dir = "game_board"
-    local depth = $1
-    local width = $2
-    local files = $3
+    local dir=$1
+    local depth=$2
+    local width=$3
+    local files=$4
 
     mkdir -p "$dir"
 
     if [ "$depth" -le 0 ]; then
-        for ((f=1;f<files; f++)); do
-            file_path= "$dir/$(random_text 10)"
+        for ((f=1;f<=files; f++)); do
+            file_path="$dir/$(random_text 3)"
+            random_text 20 > "$file_path"
         done
-    fi    
+    
+    else 
+        local i
+        for ((i=0; i<$width; i++)); do
+            dir_path="$dir/$(random_text 10)"
+            new_depth=$((depth-1))
+            create_board $dir_path $new_depth $width $files
+        done
+    fi   
 }        
 
-create_board 2 4 5
+
+# ---------------------
+
+# Setup
+dir="game_board"
+clean_board
+create_board $dir 3 4 5
+
+# Material
+#  aux 5 : para game_board es el mismo codigo
